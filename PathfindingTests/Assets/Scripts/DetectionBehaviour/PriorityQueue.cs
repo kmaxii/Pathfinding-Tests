c69a77f9747c02646042d37bdfff2a73967d516c
kmaxii <esst936@gmail.com>
2024-02-05 13:45:22 +0100

    Added Good A* and good Dijkstra

---
+++ PathfindingTests/Assets/Scripts/DetectionBehaviour/PriorityQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DetectionBehaviour
+{
+    // Priority Queue implementation (if needed)
+    public class PriorityQueue<TElement, TPriority> where TElement : notnull
+    {
+        private SortedDictionary<TPriority, Queue<TElement>> elements = new SortedDictionary<TPriority, Queue<TElement>>();
+
+        public void Enqueue(TElement item, TPriority priority)
+        {
+            if (!elements.ContainsKey(priority))
+                elements[priority] = new Queue<TElement>();
+
+            elements[priority].Enqueue(item);
+        }
+
+        public TElement Dequeue()
+        {
+            if (elements.Count == 0)
+                throw new InvalidOperationException("The queue is empty");
+
+            var firstPair = elements.First();
+            var item = firstPair.Value.Dequeue();
+            if (firstPair.Value.Count == 0)
+                elements.Remove(firstPair.Key);
+
+            return item;
+        }
+
+        public bool Contains(TElement item)
+        {
+            return elements.Any(pair => pair.Value.Contains(item));
+        }
+
+        public int Count => elements.Sum(pair => pair.Value.Count);
+    }
+}
