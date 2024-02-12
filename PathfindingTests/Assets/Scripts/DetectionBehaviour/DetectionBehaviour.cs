777187e2ce0ca376e8d9d455f5cc3c6ca0c47a76
kmaxii <esst936@gmail.com>
2024-02-12 14:46:32 +0100

    Fixed build error

--- PathfindingTests/Assets/Scripts/DetectionBehaviour/DetectionBehaviour.cs
+++ PathfindingTests/Assets/Scripts/DetectionBehaviour/DetectionBehaviour.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using MaxisGeneralPurpose.Scriptable_objects;
-using TMPro.EditorUtilities;
 using UnityEngine;
 
 namespace DetectionBehaviour
@@ -15,7 +14,8 @@ namespace DetectionBehaviour
 
         public abstract (LinkedList<Vector2Int>, int nodesExplored) GetShortestPath(Vector2Int start, Vector2Int end);
 
-        protected IEnumerable<Vector2Int> GetNeighbours(Vector2Int position) {
+        protected IEnumerable<Vector2Int> GetNeighbours(Vector2Int position)
+        {
             var neighbours = new List<Vector2Int>();
             var x = position.x;
             var y = position.y;
@@ -25,39 +25,50 @@ namespace DetectionBehaviour
 
             // Add cardinal neighbors
             // Up
-            if (y < mapHeight - 1 && mapData.CheckCoordinate(x, y + 1)) 
-                neighbours.Add(new Vector2Int(x, y + 1)); 
+            if (y < mapHeight - 1 && mapData.CheckCoordinate(x, y + 1))
+                neighbours.Add(new Vector2Int(x, y + 1));
             // Right
-            if (x < mapWidth - 1 && mapData.CheckCoordinate(x + 1, y)) 
-                neighbours.Add(new Vector2Int(x + 1, y)); 
+            if (x < mapWidth - 1 && mapData.CheckCoordinate(x + 1, y))
+                neighbours.Add(new Vector2Int(x + 1, y));
             // Down
-            if (y > 0 && mapData.CheckCoordinate(x, y - 1)) 
-                neighbours.Add(new Vector2Int(x, y - 1)); 
+            if (y > 0 && mapData.CheckCoordinate(x, y - 1))
+                neighbours.Add(new Vector2Int(x, y - 1));
             // Left
-            if (x > 0 && mapData.CheckCoordinate(x - 1, y)) 
-                neighbours.Add(new Vector2Int(x - 1, y)); 
+            if (x > 0 && mapData.CheckCoordinate(x - 1, y))
+                neighbours.Add(new Vector2Int(x - 1, y));
 
             // Add diagonal neighbours
             // Up Right
-            if (x < mapWidth - 1 && y < mapHeight - 1 && mapData.CheckCoordinate(x + 1, y + 1) && (mapData.CheckCoordinate(x + 1, y) || mapData.CheckCoordinate(x, y + 1))) {
+            if (x < mapWidth - 1 && y < mapHeight - 1 && mapData.CheckCoordinate(x + 1, y + 1) &&
+                (mapData.CheckCoordinate(x + 1, y) || mapData.CheckCoordinate(x, y + 1)))
+            {
                 neighbours.Add(new Vector2Int(x + 1, y + 1));
             }
+
             // Down Right
-            if (x < mapWidth - 1 && y > 0 && mapData.CheckCoordinate(x + 1, y - 1) && (mapData.CheckCoordinate(x + 1, y) || mapData.CheckCoordinate(x, y - 1))) {
+            if (x < mapWidth - 1 && y > 0 && mapData.CheckCoordinate(x + 1, y - 1) &&
+                (mapData.CheckCoordinate(x + 1, y) || mapData.CheckCoordinate(x, y - 1)))
+            {
                 neighbours.Add(new Vector2Int(x + 1, y - 1));
             }
+
             // Down Left
-            if (x > 0 && y > 0 && mapData.CheckCoordinate(x - 1, y - 1) && (mapData.CheckCoordinate(x - 1, y) || mapData.CheckCoordinate(x, y - 1))) {
+            if (x > 0 && y > 0 && mapData.CheckCoordinate(x - 1, y - 1) &&
+                (mapData.CheckCoordinate(x - 1, y) || mapData.CheckCoordinate(x, y - 1)))
+            {
                 neighbours.Add(new Vector2Int(x - 1, y - 1));
             }
+
             // Up Left
-            if (x > 0 && y < mapHeight - 1 && mapData.CheckCoordinate(x - 1, y + 1) && (mapData.CheckCoordinate(x - 1, y) || mapData.CheckCoordinate(x, y + 1))) {
+            if (x > 0 && y < mapHeight - 1 && mapData.CheckCoordinate(x - 1, y + 1) &&
+                (mapData.CheckCoordinate(x - 1, y) || mapData.CheckCoordinate(x, y + 1)))
+            {
                 neighbours.Add(new Vector2Int(x - 1, y + 1));
             }
-            
+
             return neighbours;
-            
-            
+
+
             /*
              *     int tX = iNode.x;
             int tY = iNode.y;
@@ -128,7 +139,8 @@ namespace DetectionBehaviour
         }
 
 
-        protected float GetDistance(Vector2Int first, Vector2Int second) {
+        protected float GetDistance(Vector2Int first, Vector2Int second)
+        {
             //Chebyshev_distance
             //return Mathf.Max(Mathf.Abs(first.x - second.x), Mathf.Abs(first.y - second.y));
             //return Mathf.Abs(first.x - second.x) + Mathf.Abs(first.y - second.y);
@@ -136,13 +148,14 @@ namespace DetectionBehaviour
             //return Mathf.Sqrt(GetDistanceSquared(first, second));
             //return GetDistanceSquared(first, second);
             return Vector2.Distance(first, second);
-            
+
             /*float num1 = Mathf.Abs(first.x - second.x);
             float num2 = Mathf.Abs(first.y - second.y);
             return num1 + num2;*/
         }
 
-        private float GetDistanceSquared(Vector2Int first, Vector2Int second) {
+        private float GetDistanceSquared(Vector2Int first, Vector2Int second)
+        {
             float num1 = first.x - second.x;
             float num2 = first.y - second.y;
             return Mathf.Abs(num1) + Mathf.Abs(num2);
